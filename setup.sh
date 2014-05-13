681434bd7bd90bcf03309142f353f8bb26f265c3
Alex Dobrushskiy <Alexey.Dobrushskiy@artezio.com>
2014-05-13 11:55:57 +0400

    Added gitconfig + edit dopfiles repo URL

--- setup.sh
+++ setup.sh
@@ -39,10 +39,10 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+git clone https://github.com/AlexDobrushskiy/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
-
+ln -sf dotfiles/.gitconfig .
